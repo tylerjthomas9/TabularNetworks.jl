8b0a3a1587216a030322b67433666232360cc63e
tylerjthomas9 <tylerjthomas9@gmail.com>
2021-08-11 18:42:12 -0500

    create files for tabnet, tabtransformer

--- src/models/tabtransformer.jl
+++ src/models/tabtransformer.jl
@@ -1,3 +1,5 @@
+# https://arxiv.org/abs/2012.06678
+
 using Flux
 using Flux: @functor
 using Flux.Data: DataLoader
@@ -6,12 +8,13 @@ using Parameters: @with_kw
 using CUDA
 
 # Struct to define hyperparameters
-@with_kw mutable struct Args
+@with_kw mutable struct TabTransfortmerArgs
     lr::Float64 = 0.1		# learning rate
     batchsize::Int64 = 16  # batch size
     epochs::Int = 10        # number of epochs
     use_cuda::Bool = true   # use gpu (if cuda available)
     dropout::Float64 = 0.10 # dropout from dense layers
-    hidden_size::Vector{Int64} = [64, 64] # Size of hidden layers
+    hidden_sizes::Vector{Int64} = [64, 64] # Size of hidden layers
+    dropout_rate::Float64 = 0.10 # dropout for dense layers
     activation_function = relu
 end
