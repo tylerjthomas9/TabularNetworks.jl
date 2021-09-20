08efcc1584586e52eeb7e4490184b8e5833a6bd9
tylerjthomas9 <tylerjthomas9@gmail.com>
2021-09-20 13:56:45 -0500

    move transformer layer to its own script for reuse

--- src/models/tabtransformer.jl
+++ src/models/tabtransformer.jl
@@ -9,7 +9,7 @@ using CUDA
 using NeuralAttentionlib
 using NeuralAttentionlib: CausalMask, BiLengthMask, BatchedMask
 using NNlib
-include("../utils/mha.jl")
+include("../layers/transformer.jl")
 
 # Struct to define hyperparameters
 @with_kw mutable struct TabTransfortmerArgs
@@ -31,59 +31,6 @@ include("../utils/mha.jl")
     transformer_dropout::Float64 = 0.1
     transformer_dense_hidden_dim::Int64 = 64
     seed::Int64 = 42
-end
-
-#https://github.com/chengchingwen/Transformers.jl/blob/master/src/basic/transformer.jl
-struct TransformerDense{Di<:Dense, Do<:Dense}
-    din::Di
-    dout::Do
-end
-
-@functor TransformerDense
-
-
-"just a wrapper for two dense layer."
-TransformerDense(size::Int, h::Int, act = relu) = TransformerDense(
-    Dense(size, h, act),
-    Dense(h, size)
-)
-
-function (pw::TransformerDense)(x::AbstractMatrix)
-  # size(x) == (dims, seq_len)
-  return pw.dout(pw.din(x))
-end
-
-function (d::TransformerDense)(x::A) where {T, N, A<:AbstractArray{T, N}}
-  new_x = reshape(x, size(x, 1), :)
-  y = d(new_x)
-  return reshape(y, Base.setindex(size(x), size(y, 1), 1))
-end
-
-
-struct Transformer
-    mha
-    ln
-    dense
-end
-
-@functor Transformer
-
-Transformer(args::TabTransfortmerArgs) = Transformer(
-    MultiheadAttention(args.mha_heads, args.cat_input_dim, args.mha_head_dims, 
-                                        args.cat_input_dim; future = true, pdrop = args.transformer_dropout),
-    LayerNorm(args.cat_input_dim),
-    Chain(TransformerDense(args.cat_input_dim, args.transformer_dense_hidden_dim, args.activation_function), 
-            Dropout(args.transformer_dropout))
-)
-
-function (t::Transformer)(x)
-    h1 = atten(t.mha, x)
-    h2 = x + h1
-    h2 = t.ln(h2)
-    h3 = t.dense(h2)
-    h4 = h2 + h3
-    h4 = t.ln(h4)
-    return h4
 end
 
 struct TabTransformer
